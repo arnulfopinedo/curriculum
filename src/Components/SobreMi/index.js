75d29e071b5b9a336091fb3695e392a0d1fe5d8b
arnulfopinedo <arnulfo.angel2017@gmail.com>
2025-12-30 05:24:16 -0600

    Actualizando sobre mi

--- src/Components/SobreMi/index.js
+++ src/Components/SobreMi/index.js
@@ -5,7 +5,7 @@ const SobreMi = () => {
                 <div className="sobremi">
                     <h2 id="sobremi">Sobre mi</h2>
                     <p>
-                        Maestro en sistemas computacionales con conocimiento en desarrollo de aplicaciones web, experiencia en HTML, CSS, Bootstrap, PHP, WordPress, Mobirise, Shopify, GitHub, JavaScript, JQuery, Axios, Node, Typescript, React. Comprometido en implementar soluciones innovadoras que optimicen el rendimiento del sistema. Proactivo, experimentado, orientado a resultados, capacidad de liderazgo, eficiente, colaborador, creativo y sobre todo enfocado en la solución de problemas.
+                        Maestro en sistemas computacionales con conocimiento en desarrollo de aplicaciones web, experiencia en HTML, CSS, Bootstrap, PHP, WordPress, Mobirise, Shopify, Moodle, GitHub, JavaScript, JQuery, Axios, Node, Typescript, React. Comprometido en implementar soluciones innovadoras que optimicen el rendimiento del sistema. Proactivo, experimentado, orientado a resultados, capacidad de liderazgo, eficiente, colaborador, creativo y sobre todo enfocado en la solución de problemas.
                     </p>
                 </div>
             </article>
